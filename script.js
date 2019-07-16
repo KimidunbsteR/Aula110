1d229f0398449eb9f3b190eb1ba31631a33c6c48
KimidunbsteR <wgzmesquita@gmail.com>
2019-07-16 11:07:31 -0300

    tentativa 03

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-let memoria
+function embaralhar(lista) {
+    let valorTemporario;
+    let indiceAleatorio;
+
+    for (let i =)
+}
+
+let imagens = ["2pac.jpg", "50cent.jpg", "biggie.jpg", "chris-brown.jpg", "eazy-e.jpg", "eminem.jpg", "ice-cube.jpg", "snoop-dogg.jpg"]
+
+let imagens = imagensSalvas.concat(imagensSalvas);
+
+imagens = embaralhar(imagens);
+
+for(let i in cartas){
+    cartas[i].style.back
+}
